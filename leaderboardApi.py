b7b7d8b0d26d35281272f9fe1a195f3d684201cf
“aratimauryacsuf” <aratimaurya@csu.fullerton.edu>
2022-11-22 21:30:30 -0800

    updated leaderboard api

--- leaderboardApi.py
+++ leaderboardApi.py
@@ -11,6 +11,7 @@ QuartSchema(app)
 # app.config.from_file(f"./etc/{__name__}.toml", toml.load)
 
 # game_result=redis.Redis(host="http://127.0.0.1", port=5200,db=0)
+game_result=redis.Redis(host="localhost", port=6379)
 
 @dataclasses.dataclass
 class result:
@@ -31,11 +32,14 @@ def index():
 @app.route("/report_result", methods=["POST"])
 # @validate_request(result)
 def report_result():
-    result ={"username":"ABC", "game_status":"win", "guess_count":5}
-    print(result)
-    game_result=redis.Redis(host="localhost", port=5200,db=0)
-    outcome=game_result.mset({"username":result["username"], "game_status":result["game_status"],"guess_count":result["guess_count"] })
-    print("outcome"+outcome)
+    # # result ={"username":"PQR", "game_status":"WIN", "guess_count":4}
+    # print(result)
+   
+    # outcome=game_result.mset({"username":result["username"], "game_status":result["game_status"],"guess_count":result["guess_count"] })
+    keys = game_result.keys()
+    print(keys)
+    # testresult=game_result.GETSET("username")
+    # print(testresult)
 
 
 
