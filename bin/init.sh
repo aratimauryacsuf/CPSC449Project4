6bc695daa6dbcb0e9a989e2235afaa30cde0b0b8
Ayush Bhardwaj <100165141+Ayush-corp@users.noreply.github.com>
2022-12-03 11:45:18 -0800

    Updated Redis Initialization

--- bin/init.sh
+++ bin/init.sh
@@ -4,3 +4,4 @@ sqlite3 ./var/game_primary/mount/wordleGame.db< ./share/wordlegameschema.sql
 sqlite3 ./var/wordleUser.db< ./share/wordleuserschema.sql
 
 python3 ./bin/copydata.py
+python3  ./bin/redis_initialization.py
